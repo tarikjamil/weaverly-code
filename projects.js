01846ca7e3a3bda5591f0973ceeea39b14cd1f54
tarikjamil <tarikjamill@gmail.com>
2023-09-13 09:05:04 +0100

    add projects

---
+++ projects.js
@@ -0,0 +1,20 @@
+$(".richtext-project *").each(function (index) {
+  let target = $(this);
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: $(this),
+      start: "top bottom -=200",
+    },
+  });
+
+  tl.from(
+    target,
+    {
+      y: "20rem",
+      opacity: 0,
+      ease: "Quint.easeOut",
+      duration: 1,
+    },
+    0
+  );
+});
