f1392d6705605e04be0183b6f0ed8cf554d0a9f9
tarikjamil <tarikjamill@gmail.com>
2023-09-13 09:43:55 +0100

    edits

---
+++ home.js
@@ -0,0 +1,19 @@
+// slider 1
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".is--home-outside", {
+    type: "slide",
+    perPage: 1,
+    perMove: 1,
+    gap: "24rem",
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      991: {
+        // Tablet
+        gap: "24rem",
+        drag: true,
+      },
+    },
+  });
+  splide.mount();
+});
