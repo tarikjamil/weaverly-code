9c352d53f9b0a634e067e8dc06f3ce2ea8b9c74a
tarikjamil <tarikjamill@gmail.com>
2023-09-13 09:06:42 +0100

    Update projects.js

--- projects.js
+++ projects.js
@@ -18,3 +18,24 @@ $(".richtext-project *").each(function (index) {
     0
   );
 });
+
+$(".richtext *").each(function (index) {
+  let target = $(this);
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: $(this),
+      start: "top bottom -=200",
+    },
+  });
+
+  tl.from(
+    target,
+    {
+      y: "20rem",
+      opacity: 0,
+      ease: "Quint.easeOut",
+      duration: 1,
+    },
+    0
+  );
+});
