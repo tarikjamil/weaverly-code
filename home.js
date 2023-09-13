c1b21057d5b0370b8b898373f7ccea5932e68df8
tarikjamil <tarikjamill@gmail.com>
2023-09-13 10:32:36 +0100

    Update home.js

--- home.js
+++ home.js
@@ -17,3 +17,22 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   splide.mount();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".is--home-numbers", {
+    type: "slide",
+    perPage: 1,
+    perMove: 1,
+    gap: "24rem",
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      991: {
+        // Tablet
+        gap: "24rem",
+        pagination: true,
+      },
+    },
+  });
+  splide.mount();
+});
